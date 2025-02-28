f57f026a89df393a567398d6158b32347196896c
Flow Earn <hi@flow.fans>
2025-02-28 23:05:40 +0800

    fix: add export output config for static deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   webpack: (config) => {
     config.externals.push({
       'utf-8-validate': 'commonjs utf-8-validate',
