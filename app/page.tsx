2d14c13bed29ef904fdb7a8f60a6d008ce59a729
Flow Earn <hi@flow.fans>
2025-02-28 20:36:45 +0800

    feat: update layout and styling to match flow-dashboard design

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,65 @@
 'use client';
 
-import React from 'react';
 import KlineChart from './components/KlineChart';
 import OrderBook from './components/OrderBook';
 import TradeVolume from './components/TradeVolume';
 import FundingRate from './components/FundingRate';
 import VolumePulse from './components/VolumePulse';
 import LiquidationPoints from './components/LiquidationPoints';
+import PriceChange from './components/PriceChange';
 import AskMeAnything from './components/AskMeAnything';
 
 export default function Home() {
   return (
-    <main className="container mx-auto p-4">
-      <div className="space-y-4">
-        <KlineChart />
+    <main>
+      {/* Banner */}
+      <div className="banner text-white">
+        <div className="container flex items-center">
+          <img src="/flow-logo.png" alt="Flow" className="h-12 mr-4" />
+          <h1 className="text-2xl font-bold">Flow</h1>
+        </div>
+      </div>
+
+      <div className="container py-4">
+        {/* Kline Chart */}
+        <div className="box">
+          <KlineChart />
+        </div>
+
+        {/* Order Book & Trade Volume Classification */}
         <div className="grid grid-cols-2 gap-4">
-          <OrderBook />
-          <TradeVolume />
+          <div className="box">
+            <OrderBook />
+          </div>
+          <div className="box">
+            <TradeVolume />
+          </div>
         </div>
-        <div className="grid grid-cols-3 gap-4">
-          <FundingRate />
-          <VolumePulse />
-          <LiquidationPoints />
+
+        {/* Funding Rate & Volume Pulse */}
+        <div className="grid grid-cols-2 gap-4">
+          <div className="box">
+            <FundingRate />
+          </div>
+          <div className="box">
+            <VolumePulse />
+          </div>
         </div>
-        <AskMeAnything />
+
+        {/* Liquidation Points & 24h Change */}
+        <div className="grid grid-cols-2 gap-4">
+          <div className="box">
+            <LiquidationPoints />
+          </div>
+          <div className="box">
+            <PriceChange />
+          </div>
+        </div>
+
+        {/* Ask Me Anything */}
+        <div className="box">
+          <AskMeAnything />
+        </div>
       </div>
     </main>
   );
